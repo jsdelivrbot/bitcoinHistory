54857b57f9dc514e593506d7ecc5e44d685e2b70
Amodar <ajaysingh@live.nl>
2015-02-05 21:29:46 +0100

    formatting

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 (function () {
 
     var request = new XMLHttpRequest();
-    request.open('GET', 'http://broowse.com/history.json', true);
+    request.open('GET', 'http://amodar.github.io/bitcoinHistory/history.json', true);
     
     request.onload = function() {
         if (request.status >= 200 && request.status < 400) {
-            // Success!
+            
             var jsonData = JSON.parse(request.responseText);
             var bitcoinData = [];
             var average = [];
             var high = [];
             var low = [];
             var dates = [];
-            //turn json to js array
+            
             for (var i = 0; i < jsonData.length; i++) {
                 var date = new Date(jsonData[i].datetime);
                 
@@ -28,52 +28,42 @@
             low.unshift("low");
             dates.unshift("x");
             
-            console.log(dates);
-            
-                var chart = c3.generate({
-                    data: {
-                        x: 'x',
-                        columns: [
-                            dates,
-                            low,
-                            average,
-                            high
-                        ],
-                        types: {
-                            low: 'area-step',
-                            average: 'area-step',
-                            high: 'area-step'
-                            // 'line', 'spline', 'step', 'area', 'area-step' are also available to stack
-                        },
-                        groups: [['low', 'average', 'high']],
-                        order: false
+            var chart = c3.generate({
+                data: {
+                    x: 'x',
+                    columns: [
+                        dates,
+                        low,
+                        average,
+                        high
+                    ],
+                    types: {
+                        low: 'area-step',
+                        average: 'area-step',
+                        high: 'area-step'
                     },
-                    axis: {
-                        x: {
-                            type: 'timeseries',
-                            // if true, treat x value as localtime (Default)
-                            // if false, convert to UTC internally
-                            localtime: false,
-                            tick: {
-                                format: '%Y-%m-%d'
-                            }
+                    groups: [['low', 'average', 'high']],
+                    order: false
+                },
+                axis: {
+                    x: {
+                        type: 'timeseries',
+                        localtime: false,
+                        tick: {
+                            format: '%Y-%m-%d'
                         }
                     }
-                    
-                    
-                    
-                    
-                });            
-            
-            
+                }
+            });
         } else {
             // We reached our target server, but it returned an error
-            
+            console.log('target server error');
         }
     };
     
     request.onerror = function() {
         // There was a connection error of some sort
+        console.log('request error');
     };
     
     request.send();
